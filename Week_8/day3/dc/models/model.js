30c10d193a90318778abeb80a7d9f58c6eea50dc
David <51931592+dvdzmr@users.noreply.github.com>
2024-04-17 13:22:06 +0300

    big trouble focussing so im calling it a day.

--- Week_8/day3/dc/models/model.js
+++ Week_8/day3/dc/models/model.js
@@ -1,10 +1,41 @@
-import db from "../config/config.js"
+// import db from "../config/config.js"
+import db from "../users.json"
+import { writeFile } from 'fs';
 
 const User = {
-    getAll: () => db('users').select('*'),
-    getById: (id) => db('users').where({ id }).first(),
-    create: (user) => db('users').insert(user).returning('*'),
-    update: (id, user) => db('users').where({ id }).update(user).returning('*'),
-};
+    create: createTask(username, password),
+    // getByUsername: 
+    
+}
+
+
+function createTask(username, password) {
+    const task = {
+        username,
+        password
+      };
+
+      db.push(task);
+      writeJson();
+}
+
+
+function writeJson(task) {
+    writeFile(db, JSON.stringify(task, null, 2), (error) => {
+        if (error) {
+          console.log('An error has occurred ', error);
+          return;
+        }
+        console.log('Data written successfully to disk');
+    });
+
+}
+
+// const User = {
+//     getAll: () => db('users').select('*'),
+//     getById: (id) => db('users').where({ id }).first(),
+//     create: (user) => db('users').insert(user).returning('*'),
+//     update: (id, user) => db('users').where({ id }).update(user).returning('*'),
+// };
 
 export default User;
