0623c914bd7e72f88ef5bc19e7f217f3ce1606d1
David <51931592+dvdzmr@users.noreply.github.com>
2024-07-05 12:18:10 +0300

    comitting previously uncomitted work day 4 week 11

--- Week_11/day4/app.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import router from "./routes/index.js";
-
-const app = express();
-app.use(express.json());
-app.use("/", router);
-
-app.listen(3000, () => console.log("Hosting on 127.0.0.1:3000"));
